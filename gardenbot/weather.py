7ca2d6e152591c89437655ceca2b641f860d4f1b
Daniel DeSousa <github@daniel.desousa.cc>
2016-08-03 15:41:31 -0400

    some more unit tests for forecasted weather

--- gardenbot/weather.py
+++ gardenbot/weather.py
@@ -53,13 +53,17 @@ class ForecastIODataSource(object):
 
     @lru_cache(maxsize=cache_size)
     def _get_historical_data(self, location, d):
+        # TODO: set excludes for more efficient requests
         query_params = dict(exclude="[]")
         url = self._url_with_qargs(self.time_machine_url_template, location, d, query_params)
         return requests.get(url)
 
     @lru_cache(maxsize=cache_size)
-    def _get_forecast_data(self, location, d):
-        pass
+    def _get_forecast_data(self, location):
+        # TODO: set excludes for more efficient requests
+        query_params = dict(exclude="[]")
+        url = self._url_with_qargs(self.forecast_url_template, location, query_params=query_params)
+        return requests.get(url)
 
     def _get_historical_rainfall(self, location, d):
         response = self._get_historical_data(location, d)
@@ -67,14 +71,22 @@ class ForecastIODataSource(object):
             return None
 
         hourly_data = response.json()['hourly']['data']
-        accumulated_rainfall = sum(hr['precipIntensity'] for hr in hourly_data if hr['precipType'] == 'rain')
+        accumulated_rainfall = sum(hr['precipIntensity'] for hr in hourly_data if hr.get('precipType') == 'rain')
         return accumulated_rainfall
 
-    def _get_forecasted_rainfall(self, location, d):
-        pass
+    def _get_forecasted_rainfall(self, location):
+        response = self._get_forecast_data(location)
+        if response.status_code != 200:
+            return None
+
+        hourly_data = response.json()['hourly']['data']
+        # can we make a more accurate prediction than the best case?
+        # we need to look more than two days in the future
+        accumulated_rainfall = sum(hr['precipIntensity'] for hr in hourly_data if hr.get('precipType') == 'rain')
+        return accumulated_rainfall
 
     def get_rainfall(self, location, d):
         if d <= date.today():
             return self._get_historical_rainfall(location, d)
         else:
-            return self._get_forecasted_rainfall(self, location, d)
+            return self._get_forecasted_rainfall(location)
--- tests/test_forecast_io.py
+++ tests/test_forecast_io.py
@@ -10,9 +10,16 @@ from datetime import date, timedelta
 from unittest.mock import Mock, patch
 from gardenbot.weather import ForecastIODataSource
 
+
+# historical fixture
 historical_filename = os.path.join(os.path.dirname(__file__), "response_data", "historical_response.json")
 with open(historical_filename, "rt") as f:
     HISTORICAL_RESPONSE = Mock(status_code=200, json=Mock(return_value=json.load(f)))
+
+# forecast fixture
+forecast_filename = os.path.join(os.path.dirname(__file__), "response_data", "forecast_response.json")
+with open(forecast_filename, "rt") as f:
+    FORECAST_RESPONSE = Mock(status_code=200, json=Mock(return_value=json.load(f)))
 
 
 class TestForecastIODataSource(unittest.TestCase):
@@ -24,8 +31,23 @@ class TestForecastIODataSource(unittest.TestCase):
         pass
 
     @patch('gardenbot.weather.ForecastIODataSource._get_historical_data', return_value=HISTORICAL_RESPONSE)
-    def test_historical_rainfall(self, *mocks):
+    def test_historical_rainfall(self, get_data_mock):
         yesterday = date.today() - timedelta(days=1)
         location = Mock(latitude=44.0, longitude=44.0)
         actual = self.data_source.get_rainfall(location, yesterday)
         self.assertIsNotNone(actual)
+        self.assertEqual(1, len(get_data_mock.mock_calls))
+
+    @patch('gardenbot.weather.ForecastIODataSource._get_historical_data', return_value=HISTORICAL_RESPONSE)
+    def test_historical_rainfall_equal(self, get_data_mock):
+        location = Mock(latitude=44.0, longitude=44.0)
+        self.data_source.get_rainfall(location, date.today())
+        self.assertEqual(1, len(get_data_mock.mock_calls))
+
+    @patch('gardenbot.weather.ForecastIODataSource._get_forecast_data', return_value=FORECAST_RESPONSE)
+    def test_forecasted_rainfall(self, get_data_mock):
+        tomorrow = date.today() + timedelta(days=1)
+        location = Mock(latitude=44.0, longitude=44.0)
+        actual = self.data_source.get_rainfall(location, tomorrow)
+        self.assertIsNotNone(actual)
+        self.assertEqual(1, len(get_data_mock.mock_calls))
