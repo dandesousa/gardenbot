43e1875e5cc27b4234fe247de4894ab2f03ad831
Daniel DeSousa <github@daniel.desousa.cc>
2016-08-03 15:17:23 -0400

    adding test for historical responses

--- tests/test_forecast_io.py
+++ tests/test_forecast_io.py
@@ -5,13 +5,15 @@
 import json
 import os
 import unittest
-import unittest.mock as mock
 
+from datetime import date, timedelta
+from unittest.mock import Mock, patch
 from gardenbot.weather import ForecastIODataSource
 
 historical_filename = os.path.join(os.path.dirname(__file__), "response_data", "historical_response.json")
 with open(historical_filename, "rt") as f:
-    HISTORICAL_DATA = json.load(f)
+    HISTORICAL_RESPONSE = Mock(status_code=200, json=Mock(return_value=json.load(f)))
+
 
 class TestForecastIODataSource(unittest.TestCase):
 
@@ -21,6 +23,10 @@ class TestForecastIODataSource(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @mock.patch('gardenbot.weather.ForecastIODataSource._get_historical_data', return_value=HISTORICAL_DATA)
+    @patch('gardenbot.weather.ForecastIODataSource._get_historical_data', return_value=HISTORICAL_RESPONSE)
     def test_historical_rainfall(self, *mocks):
-        pass
+        yesterday = date.today() - timedelta(days=1)
+        location = Mock(latitude=44.0, longitude=44.0)
+        import pdb; pdb.set_trace()
+        actual = self.data_source.get_rainfall(location, yesterday)
+        self.assertIsNotNone(actual)
